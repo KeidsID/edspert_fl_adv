1ccd6ad68ded16089c38710543f62fbdc0fc2c40
KeidsID <keidscode@gmail.com>
2023-12-11 20:37:58 +0800

    lint: remove unused import

--- lib/interfaces/views/home_shell/home_layout.dart
+++ lib/interfaces/views/home_shell/home_layout.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:root_lib/interfaces/providers.dart';
 import 'package:root_lib/interfaces/router/routes/routes.dart';
 
 const _navs = <_NavDelegate>[
